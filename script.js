df9999b6cf830cf38cd837224f86b4bfbebf7ce2
MessaoudiAya <127698243+MessaoudiAya@users.noreply.github.com>
2023-04-01 01:03:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-(function () {
+var names=new Array();
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
+names[3]="jason";
+names[4]="paul";
+names[5]="frank";
+names[6]="larry";
+names[7]="paula";
+names[8]="laura";
+names[9]="jim";
 
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-
-    for (var name in names) {
-
-       
-        var firstLetter = names[name].charAt(0).toLowerCase();
-
-        
-        if (firstLetter === 'j') {
-            byeSpeaker.speak(names[name]);
-        } else {
-            helloSpeaker.speak(names[name]);
-        }
-    }
-})();
+for (var i = 0; i < names.length; i++) {
+	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
+        console.log("Goodbye "+ names[i])
+	}
+	else{
+		console.log("Hello "+ names[i])
+	}
 
